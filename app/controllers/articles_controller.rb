53ec0e59f187ad6d3b3ef80d84c9e643ab1a8ac3
k-boiteau <boiteau.kev@gmail.com>
2017-05-16 14:44:42 +0200

    wiki update

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,23 @@
+class ArticlesController < ApplicationController
+  def list
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
